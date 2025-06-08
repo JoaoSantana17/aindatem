722421118885f74158d30ec445cce59ce4d8efde
1TDSPR-CP <joao.lsantana17@gmail.com>
2025-06-08 20:13:31 -0300

    Cadastro de novo usuário

--- cadastro/page.tsx
+++ cadastro/page.tsx
@@ -73,7 +73,7 @@ export default function CadastroPage() {
             className="w-full px-4 py-3 rounded-xl border-2 border-gray-300 bg-white text-gray-900 focus:border-blue-500"
             disabled={loading}
           />
-
+ 
           <input
             type="password"
             value={senha}
